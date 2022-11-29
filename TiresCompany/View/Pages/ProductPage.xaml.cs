9c56c23a8a6f306e8e3f4c574ae29719c4431a4e
rotoyouoi <rotoyouoi@yandex.ru>
2022-11-29 11:08:36 +0500

    Макет страницы

--- TiresCompany/View/Pages/ProductPage.xaml.cs
+++ TiresCompany/View/Pages/ProductPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TiresCompany.Model;
 
 namespace TiresCompany.View.Pages
 {
@@ -20,9 +21,11 @@ namespace TiresCompany.View.Pages
     /// </summary>
     public partial class ProductPage : Page
     {
+        Core db = new Core();
         public ProductPage()
         {
             InitializeComponent();
+            ProductListView.ItemsSource = db.context.Product.ToList();
         }
     }
 }
